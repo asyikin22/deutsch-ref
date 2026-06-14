d5a27b0de1f5a63f66e05709706b4ccaa5b6874a
Asyikin <148519441+asyikin22@users.noreply.github.com>
2026-06-14 17:24:49 +0800

    cleared cache to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'deutsch-v1';
+const CACHE = 'deutsch-v2';
 const FILES = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
